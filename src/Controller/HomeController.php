a2edf06c0551100b0e91e3af224260dde450020f
qawelo <62259772+qawelo@users.noreply.github.com>
2024-04-16 18:08:22 +0200

    ...

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class HomePage extends AbstractController
+class HomeController extends AbstractController
 {
+    #[Route(path: '/')]
     public function test(): Response
     {
         return new Response("sas");
